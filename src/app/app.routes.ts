19784caf75830e56e8061bb342e86b23a65cc67c
Haresh_thumar <haresh.thumar@artemhealthtech.com>
2024-07-22 10:28:44 +0530

    my first project init

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'groupBy', pathMatch: 'full' },
+
+  /*---------------------------------------------------------------------------
+                              Aggregate Component
+  ---------------------------------------------------------------------------*/
+  { path: 'groupBy' },
+];
